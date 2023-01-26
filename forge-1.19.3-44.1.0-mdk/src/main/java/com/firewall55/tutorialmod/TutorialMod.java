0afb5d4ac170f84405680470b47e04a397bc8444
FireWall55 <nakulgupta.usa@gmail.com>
2023-01-26 16:47:13 -0500

    First state - GitHub Check2

--- forge-1.19.3-44.1.0-mdk/src/main/java/com/firewall55/tutorialmod/TutorialMod.java
+++ forge-1.19.3-44.1.0-mdk/src/main/java/com/firewall55/tutorialmod/TutorialMod.java
@@ -10,7 +10,7 @@ import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
-//Important Comment
+//Very Important Comment
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(TutorialMod.MOD_ID)
 public class TutorialMod {
