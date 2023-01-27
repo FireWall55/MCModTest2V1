87101f6b4febe210e585f666700c3a3ca086f52e
FireWall55 <nakulgupta.usa@gmail.com>
2023-01-26 20:08:00 -0500

    Black Opal Items added into Ingredients Tab

--- forge-1.19.3-44.1.0-mdk/src/main/java/com/firewall55/tutorialmod/TutorialMod.java
+++ forge-1.19.3-44.1.0-mdk/src/main/java/com/firewall55/tutorialmod/TutorialMod.java
@@ -1,8 +1,11 @@
 package com.firewall55.tutorialmod;
 
+import com.firewall55.tutorialmod.item.ModItems;
 import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.CreativeModeTabEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -10,7 +13,6 @@ import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
-//Very Important Comment
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(TutorialMod.MOD_ID)
 public class TutorialMod {
@@ -20,9 +22,12 @@ public class TutorialMod {
     public TutorialMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
-        modEventBus.addListener(this::commonSetup);
+        ModItems.register(modEventBus);
 
+        modEventBus.addListener(this::commonSetup);
         MinecraftForge.EVENT_BUS.register(this);
+
+        modEventBus.addListener(this::addCreative);
 
     }
 
@@ -30,6 +35,12 @@ public class TutorialMod {
 
     }
 
+    private void addCreative(CreativeModeTabEvent.BuildContents event){
+        if(event.getTab() == CreativeModeTabs.INGREDIENTS){
+            event.accept(ModItems.BLACK_OPAL);
+            event.accept(ModItems.RAW_BLACK_OPAL);
+        }
+    }
 
     // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
     @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
